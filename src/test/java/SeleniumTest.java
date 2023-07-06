b49125223843b9ea9cf041be6a2904231ab29da9
LiquidPlummer <kyle.plummer@revature.com>
2023-07-06 16:53:47 -0400

    config and chromedriver

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -9,80 +9,87 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class SeleniumTest {
 
-    private WebDriver driver;
+    private WebDriver webDriver;
 
     @Before
     public void setUp() {
         // Set up ChromeDriver path
-        System.setProperty("webdriver.chrome.driver", "./chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "/driver/chromedriver");//linux_64
+
+        // Get file
+        File file = new File("TruthyFalsy.html");
+        String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
-        driver = new ChromeDriver();
-        File file = new File("TruthyFalsy.html");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("headless");
+        webDriver = new ChromeDriver(options);
+
         // Open the HTML file
-        driver.get(file.getAbsolutePath());
+        webDriver.get(path);
     }
     
     @Test
     public void testR1() {
-        WebElement r1 = driver.findElement(By.id("r1"));
+        WebElement r1 = webDriver.findElement(By.id("r1"));
         assertEquals(r1.getText(), "true");
     }
 
     @Test
     public void testR2() {
-        WebElement r2 = driver.findElement(By.id("r2"));
+        WebElement r2 = webDriver.findElement(By.id("r2"));
         assertEquals(r2.getText(), "false");
     }
 
     @Test
     public void testR3() {
-        WebElement r3 = driver.findElement(By.id("r3"));
+        WebElement r3 = webDriver.findElement(By.id("r3"));
         assertEquals(r3.getText(), "false");
     }
 
     @Test
     public void testR4() {
-        WebElement r4 = driver.findElement(By.id("r4"));
+        WebElement r4 = webDriver.findElement(By.id("r4"));
         assertEquals(r4.getText(), "true");
     }
 
     @Test
     public void testR5() {
-        WebElement r5 = driver.findElement(By.id("r5"));
+        WebElement r5 = webDriver.findElement(By.id("r5"));
         assertEquals(r5.getText(), "false");
     }
 
     @Test
     public void testR6() {
-        WebElement r6 = driver.findElement(By.id("r6"));
+        WebElement r6 = webDriver.findElement(By.id("r6"));
         assertEquals(r6.getText(), "true");
     }
 
     @Test
     public void testR7() {
-        WebElement r7 = driver.findElement(By.id("r7"));
+        WebElement r7 = webDriver.findElement(By.id("r7"));
         assertEquals(r7.getText(), "true");
     }
 
     @Test
     public void testR8() {
-        WebElement r8 = driver.findElement(By.id("r8"));
+        WebElement r8 = webDriver.findElement(By.id("r8"));
         assertEquals(r8.getText(), "true");
     }
 
     @Test
     public void testR9() {
-        WebElement r9 = driver.findElement(By.id("r9"));
+        WebElement r9 = webDriver.findElement(By.id("r9"));
         assertEquals(r9.getText(), "false");
     }
 
     @Test
     public void testR10() {
-        WebElement r10 = driver.findElement(By.id("r10"));
+        WebElement r10 = webDriver.findElement(By.id("r10"));
         assertEquals(r10.getText(), "false");
     }
 
@@ -91,6 +98,6 @@ public class SeleniumTest {
     @After
     public void tearDown() {
         // Close the browser
-        driver.quit();
+        webDriver.quit();
     }
 }
