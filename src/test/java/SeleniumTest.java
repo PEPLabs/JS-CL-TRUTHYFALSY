2cde5c23c554a45abdbd0cf5b070374de55c89ac
Revathi-Kamineni <104614509+Revathi-Kamineni@users.noreply.github.com>
2024-07-17 14:07:57 +0530

    Update and rename TruthyFalsyTest.java to SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 
-public class TruthyFalsyTest {
+public class SeleniumTest {
 
     private WebDriver webDriver;
 
